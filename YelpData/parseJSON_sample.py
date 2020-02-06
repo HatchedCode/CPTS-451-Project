c8e510a40065c563d12499f62839499b741a358c
Marco Arceo <marcoarceo@outlook.com>
2020-02-05 16:27:34 -0800

    Parsed the data to match the provided output file, need to confirm that the parse is correct

--- YelpData/parseJSON_sample.py
+++ YelpData/parseJSON_sample.py
@@ -3,15 +3,37 @@ import json
 def cleanStr4SQL(s):
     return s.replace("'","`").replace("\n"," ")
 
+def attParser(mydict, file):
+    mylist = []
+    for key, item in mydict.items():
+        if type(item) == dict:
+            for key2, item2 in item.items():
+                mylist.append(tuple((str(key2), str(item2))))      
+        else:
+            mylist.append(tuple((str(key), str(item))))
+    file.write(str(mylist) + "\n")
+    
+def hourParser(mydict, file):
+    mylist = []
+    for key, item in mydict.items():
+        str(item)
+        mylist.append(tuple((str(key), item.split('-'))))        
+    file.write(str(mylist) + "\n")
+
 def parseBusinessData():
     #read the JSON file
+    #{business_id: str, name: str, address: str, city: str, state: str, postal: str,
+    # lat: float, long: float, stars: float, rev_count: int, open: int, attributes: dict,
+    # categories: str, hours: dict}
     with open('yelp_business.JSON','r') as f:  #Assumes that the data files are available in the current directory. If not, you should set the path for the yelp data files.
         outfile =  open('business.txt', 'w')
         line = f.readline()
         count_line = 0
         #read each JSON abject and extract data
+        outfile.write("HEADER: (business_id, name; address; state; state; city; postal_code; latitude; longitude; stars; is_open)\n")
         while line:
             data = json.loads(line)
+            outfile.write(str(count_line + 1) + "- business info: ")
             outfile.write(cleanStr4SQL(data['business_id'])+'\t') #business id
             outfile.write(cleanStr4SQL(data['name'])+'\t') #name
             outfile.write(cleanStr4SQL(data['address'])+'\t') #full_address
@@ -22,13 +44,18 @@ def parseBusinessData():
             outfile.write(str(data['longitude'])+'\t') #longitude
             outfile.write(str(data['stars'])+'\t') #stars
             outfile.write(str(data['review_count'])+'\t') #reviewcount
-            outfile.write(str(data['is_open'])+'\t') #openstatus
+            outfile.write(str(data['is_open'])+'\n') #openstatus
 
             categories = data["categories"].split(', ')
-            outfile.write(str(categories)+'\t')  #category list
-            
-            outfile.write(str([])) # write your own code to process attributes
-            outfile.write(str([])) # write your own code to process hours
+            outfile.write("\tcategories: ")
+            outfile.write(str([item for item in categories])+'\n')  #category list
+            outfile.write("\tattributes: ")
+            attParser(data['attributes'], outfile)
+            outfile.write("\thours: ")
+            hourParser(data['hours'], outfile)
+            #Attributes: Sakire output is a a list of  
+            #outfile.write(str([])) # write your own code to process attributes
+            #outfile.write(str([])) # write your own code to process hours
             outfile.write('\n');
 
             line = f.readline()
